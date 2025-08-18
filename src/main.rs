d101d4e142bfc2f8cb811cd44bf76a4636d1a790
Esteban Forero <estebanmff@outlook.com>
2025-08-17 20:37:54 -0500

    Added changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use serde::Deserialize;
+use tracing::info;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    db_host: String,
+    db_port: u16,
+    db_user: String,
+    db_password: String,
+    db_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt().init();
+
+    match dotenvy::dotenv() {
+        Ok(_) => info!("ENV variables loaded from the .env file"),
+        Err(_) => info!(".env file doesn't exist, skipping step"),
+    }
+
+    let config: Config =
+        envy::from_env().expect("Failed to load config from the environment variables");
 }
